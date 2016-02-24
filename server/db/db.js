2e3664862997c3eeaf342fd2dc607152e0cdc69a
chououtside <chouming3@gmail.com>
2016-02-24 14:53:42 -0500

    change format of db file

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,8 @@
 var pg = require('pg');
 var Sequelize = require('sequelize');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/root';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/hunt';
 
-module.exports = new Sequelize(connectionString, "root", "hackreactor");
+module.exports = new Sequelize('huntdb', 'root', '', {
+  host:'localhost',
+  dialect: 'postgres'
+});
