535cf3279164ed63e5040070e0595a76888246e5
chououtside <chouming3@gmail.com>
2016-02-25 10:54:50 -0500

    modify password in db file

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 var Sequelize = require('sequelize');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:/hunt';
 
-module.exports = new Sequelize('hunt', 'chou', '', {
+module.exports = new Sequelize('hunt', 'chou', 'hackreactor', {
   host:'localhost',
   dialect: 'postgres'
 });
