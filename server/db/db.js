43efab664066cbab73efeb332fcb252442acc846
chououtside <chouming3@gmail.com>
2016-02-25 11:17:34 -0500

    add chou

--- server/db/db.js
+++ server/db/db.js
@@ -2,4 +2,7 @@ var pg = require('pg');
 var Sequelize = require('sequelize');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:/hunt';
 
-module.exports = new Sequelize(connectionString);
+module.exports = new Sequelize('hunt2', 'chouMaster', 'hackreactor', {
+  host:'localhost',
+  dialect: 'postgres'
+});
