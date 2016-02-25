49b55070d31a6dc95597afd205de871373f0e233
chououtside <chouming3@gmail.com>
2016-02-25 13:19:10 -0500

    add root user

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 var Sequelize = require('sequelize');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:/hunt';
 
-module.exports = new Sequelize('hunt', 'hunt', 'password', {
+module.exports = new Sequelize('hunt', 'root', 'password', {
   host:'localhost',
   dialect: 'postgres'
 });
