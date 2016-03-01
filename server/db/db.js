23753ddb27026a0336bf9a4f792cf21873591b9a
chououtside <chouming3@gmail.com>
2016-02-29 17:14:15 -0800

    edit db file

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,5 @@
 var pg = require('pg');
 var Sequelize = require('sequelize');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:/hunt';
 
 module.exports = new Sequelize('hunt', 'root', 'password', {
   host:'localhost',
